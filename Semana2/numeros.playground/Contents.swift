59b5b30c06104972ea1cc42e416e9c6edbb1d517
Sergio <sanchez.sergio@tec.mx>
2019-07-08 18:59:12 -0500

    Semana 2
    Curso Swift Numero

---
+++ Semana2/numeros.playground/Contents.swift
@@ -0,0 +1,22 @@
+import UIKit
+
+var numero = 0
+
+for numero in 0...100{
+    
+    if numero % 5 == 0{
+        print("\(numero) Bingo!!!")
+    }
+    
+    if numero % 2 == 0{
+        print("\(numero) Par!!!")
+    }
+    else{
+        print("\(numero) Impar!!!")
+    }
+    
+    if numero >= 30 && numero <= 40{
+        print("\(numero) Viva Swift!!!")
+    }
+    
+}
